f9b9f9224390dd11b41ec5005b5805ddb4d259fc
MeghaJ <www.meghaj80@gmail.com>
2026-05-17 21:51:53 +0000

    update files

--- Backend/recruiter/views.py
+++ Backend/recruiter/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 
 from .models import Recruiter
 
-from jobs.models import Job
+from jobs.models import Job, User
 from applications.models import Application
 
 
--- Backend/users/urls.py
+++ Backend/users/urls.py
@@ -1,37 +1,10 @@
 from django.urls import path
-
-from .views import (
-
-    register_user,
-
-    login_user,
-
-    save_profile,
-
-    get_profile
-
-)
+from .views import register_user, login_user, save_profile, get_profile
 
 urlpatterns = [
+    path("register/", register_user),
+    path("login/", login_user),
 
-    path(
-        "register/",
-        register_user
-    ),
-
-    path(
-        "login/",
-        login_user
-    ),
-
-    path(
-        "profile/save/",
-        save_profile
-    ),
-
-    path(
-        "profile/me/",
-        get_profile
-    ),
-
+    path("profile/save/", save_profile),
+    path("profile/me/", get_profile),
 ]
