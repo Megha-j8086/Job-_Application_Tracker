c38a3d9841be6bb2de8c9e6dfb35acb90a0f6e62
MeghaJ <www.meghaj80@gmail.com>
2026-05-18 14:47:49 +0000

    update files

--- Backend/applications/serializers.py
+++ Backend/applications/serializers.py
@@ -1,13 +1,40 @@
 from rest_framework import serializers
+
 from .models import Application
 
 
 class ApplicationSerializer(serializers.ModelSerializer):
 
+    # JOB DETAILS
+    role = serializers.CharField(
+        source="job.role",
+        read_only=True
+    )
+
+    company = serializers.CharField(
+        source="job.company",
+        read_only=True
+    )
+
+    location = serializers.CharField(
+        source="job.location",
+        read_only=True
+    )
+
     class Meta:
 
         model = Application
 
-        fields = "__all__"
+        fields = [
+            "id",
+            "job",
+            "user",
+            "status",
+            "role",
+            "company",
+            "location",
+        ]
 
-        read_only_fields = ["user"]
+        read_only_fields = [
+            "user"
+        ]
