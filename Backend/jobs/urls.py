6e6a6d61f1a66c05986cbd479059db7c08bfa7ee
MeghaJ <www.meghaj80@gmail.com>
2026-05-21 21:03:53 +0000

    update files

--- Backend/jobs/urls.py
+++ Backend/jobs/urls.py
@@ -6,25 +6,24 @@
 #     path("<int:id>/update/", update_job),
 #     path("<int:id>/delete/", delete_job),
 # ]
-
 from django.urls import path
-from .views import *
+from .views import jobs_list, update_job, delete_job
 
 urlpatterns = [
 
-path(
-"jobs/",
-jobs_list
-),
+    path(
+        "",
+        jobs_list
+    ),
 
-path(
-"jobs/<int:id>/",
-update_job
-),
+    path(
+        "<int:id>/",
+        update_job
+    ),
 
-path(
-"jobs/delete/<int:id>/",
-delete_job
-),
+    path(
+        "delete/<int:id>/",
+        delete_job
+    ),
 
 ]
